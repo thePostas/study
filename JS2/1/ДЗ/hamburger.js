5872a79c3220513d8602011bdf77586dc1055371
Postas <postas87@mail.ru>
2019-02-26 16:51:51 +0300

    02_26.02

--- JS2/1/ДЗ/hamburger.js
+++ JS2/1/ДЗ/hamburger.js
@@ -6,10 +6,75 @@ var Hamburger = function (size, stuffing) {
     Hamburger.STUFFING_POTATO = [15, 10];
     Hamburger.TOPPING_MAYO = [20, 5];
     Hamburger.TOPPING_SPICE = [15, 0];
+    Hamburger.TOPPING = [];
+    this.ingridients = [];
+    this.stuffing = stuffing;
+    // if (size === 'LARGE') {
+    //     // this.array = Hamburger.SIZE_LARGE;
+    //     this.array.push(Hamburger.SIZE_LARGE);
+    // } else this.array.push(Hamburger.SIZE_SMALL);
+    // if (this.stuffing === 'cheese') {
+    //     this.array.push(Hamburger.STUFFING_CHEESE);
+    // } else if (this.stuffing === 'salad') {
+    //     this.array.push(Hamburger.STUFFING_SALAD);
+    // } else this.array.push(Hamburger.STUFFING_POTATO);
+    if (size === 'LARGE') {
+        this.ingridients.push(['LARGE', Hamburger.SIZE_LARGE]);
+    } else this.this.ingridients.push(['LARGE', Hamburger.SIZE_SMALL]);
 };
 
 Hamburger.prototype.addTopping = function (topping) {
-    this.topping = topping;
+    for (let i = 0; i < topping.length; i++) {
+        if (topping === 'spice') {
+            this.array.push(Hamburger.TOPPING_SPICE);
+        } else this.array.push(Hamburger.TOPPING_MAYO);
+    }
+};
+
+/**
+ * Убрать добавку, при условии, что она ранее была
+ * добавлена.
+ *
+ * @param topping   Тип добавки
+ * @throws {HamburgerException}  При неправильном использовании
+ */
+Hamburger.prototype.removeTopping = function (topping) {
+
+};
+/**
+ * Получить список добавок.
+ *
+ * @return {Array} Массив добавленных добавок, содержит константы
+ *                 Hamburger.TOPPING_*
+ */
+Hamburger.prototype.getToppings = function () {
+    return this.topping;
+};
+/**
+ * Узнать размер гамбургера
+ */
+Hamburger.prototype.getSize = function () {
+
+};
+/**
+ * Узнать начинку гамбургера
+ */
+Hamburger.prototype.getStuffing = function () {
+
+};
+/**
+ * Узнать цену гамбургера
+ * @return {Number} Цена в тугриках
+ */
+Hamburger.prototype.calculatePrice = function () {
+
+};
+/**
+ * Узнать калорийность
+ * @return {Number} Калорийность в калориях
+ */
+Hamburger.prototype.calculateCalories = function () {
+
 };
 
 var submit = document.getElementsByClassName('hamburger__submit')[0];
@@ -43,4 +108,7 @@ submit.addEventListener('click', function (event) {
     event.preventDefault();
     var newBurger = new Hamburger(size, stuffing);
     newBurger.addTopping(topping);
+    console.log(newBurger);
+    newBurger.calculateCalories();
+    newBurger.calculatePrice();
 });
