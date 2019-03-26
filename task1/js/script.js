bbb470ceba58e88c20945c1c09f32e0768f4745f
Postas <postas87@mail.ru>
2019-03-26 19:56:36 +0300

    03_26.03

--- task1/js/script.js
+++ task1/js/script.js
@@ -1,11 +1,24 @@
+import Vue from 'vue.min'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
+Vue.use(Vue);
+
+var router = new VueRouter({
+    routes: [
+        { path: 'red', component: red}
+    ]
+})
+
 (function trafficLight() {
-    let {timers, colors} = {timers: [10000, 3000, 15000], colors: document.getElementsByClassName('light')};
+    let {timers, colors} = {timers: [3000, 1000, 3000], colors: document.getElementsByClassName('traffic__light')};
     var current = localStorage.getItem('currentMemory') || 0;
     var seconds = localStorage.getItem('secondsMemory') || timers[current]/1000;
     var reverse = localStorage.getItem('reverseMemory') || false;
-    console.log(localStorage.getItem('currentMemory'));
     (function switchLight() {
         setInterval(function () {
+            console.log(current);
+            console.log(reverse);
                 for (let i = 0; i < 3; i++) {
                     colors[i].classList.remove('active');
                 }
@@ -14,8 +27,7 @@
                 colors[1].innerHTML = seconds;
             } else colors[1].innerHTML = '';
             seconds--;
-            localStorage.setItem('secondsMemory', seconds);
-            if (seconds < 4 && !colors[current].classList.contains('yellow')) {
+            if (seconds < 4 && !colors[current].classList.contains('traffic__light-yellow')) {
                     setTimeout(function() {colors[reverse ? 2 : 0].classList.add('active')}, 500);
                     setTimeout(function() {colors[reverse ? 2 : 0].classList.remove('active')}, 1000);
             }
@@ -34,11 +46,11 @@
                     reverse = false;
                 }
             }
-            setTimeout(function () {
-                localStorage.setItem('secondsMemory', seconds);
-                localStorage.setItem('currentMemory', current);
-                localStorage.setItem('currentReverse', reverse);
-            });
-        }, 1000)
+        }, 1000);
+        setTimeout(function () {
+            localStorage.setItem('secondsMemory', seconds);
+            localStorage.setItem('currentMemory', current);
+            localStorage.setItem('reverseMemory', reverse);
+        }, 300);
     })();
 })();
