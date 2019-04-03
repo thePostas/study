f8e1db05bbb786662b5db0ca843bd8f8958e06da
Postas <postas87@mail.ru>
2019-04-03 11:16:06 +0300

    01_03.04

--- Random/Slider 3/script.js
+++ Random/Slider 3/script.js
@@ -1,11 +1,12 @@
 (function slider() {
-    var imageBlock = document.getElementsByClassName('slider-image')[0];
-    var spanParent = document.getElementsByClassName('slider__controls')[0];
-    var forwardButton = document.getElementsByClassName('slider__forward')[0];
-    var backButton = document.getElementsByClassName('slider__back')[0];
-    var currentPosition = 0;
-
-    var xhr = new XMLHttpRequest();
+    let imageBlock = document.getElementsByClassName('slider-image')[0];
+    let spanParent = document.getElementsByClassName('slider__controls')[0];
+    let forwardButton = document.getElementsByClassName('slider__forward')[0];
+    let backButton = document.getElementsByClassName('slider__back')[0];
+    let currentPosition = 0;
+    let latency = 1000;
+    let throttled = false;
+    let xhr = new XMLHttpRequest();
     xhr.open('GET', 'images.json', true);
     xhr.send();
     xhr.onreadystatechange = function () {
@@ -16,9 +17,9 @@
             if(xhr.status !== 200) {
                 console.log('Error', xhr.status, xhr.statusText);
             } else {
-                var images = JSON.parse(xhr.responseText);
+                let images = JSON.parse(xhr.responseText);
                 for (let i = 0; i < images.length; i++) {
-                    var span = document.createElement('span');
+                    let span = document.createElement('span');
                     span.classList.add('slider__button');
                     spanParent.appendChild(span);
                     if (i === 0) {
@@ -26,59 +27,60 @@
                         imageBlock.src = images[0].url;
                     }
                 }
-                var radioButtons = document.getElementsByClassName('slider__button');
+                let radioButtons = document.getElementsByClassName('slider__button');
                 for (let i = 0; i < radioButtons.length; i++) {
-                    radioButtons[i].addEventListener('click', function (callback) {
-                        currentPosition = i;
-                        slide();
+                    radioButtons[i].addEventListener('click', function () {
+                        console.log(currentPosition);
+                        if (i !== currentPosition && !throttled) {
+                            currentPosition = i;
+                            throttle(slide, latency);
+                        }
                     });
                 }
                 forwardButton.addEventListener('click', function () {
                     if (currentPosition !== radioButtons.length-1) {
-                        currentPosition++;
-                        slide();
+                        if (!throttled) {
+                            currentPosition++;
+                            throttle(slide, latency);
+                        }
                     }
                 });
 
                 backButton.addEventListener('click', function () {
-                    if (currentPosition !== 0) {
+                    if (!throttled) {
                         currentPosition--;
-                        slide()
+                        throttle(slide, latency);
                     }
                 });
                 function slide() {
                     imageBlock.classList.add('disappear');
+                    for (let i = 0; i < radioButtons.length; i++) {
+                        radioButtons[i].classList.remove('slider__button_active');
+                    }
+                    radioButtons[currentPosition].classList.add('slider__button_active');
+                    if (currentPosition === 0) {
+                        backButton.classList.remove('slider__arrow_active');
+                    } else if (currentPosition === radioButtons.length - 1) {
+                        forwardButton.classList.remove('slider__arrow_active');
+                    } else if (currentPosition !== 0 && currentPosition !== radioButtons.length - 1) {
+                        backButton.classList.add('slider__arrow_active');forwardButton.classList.add('slider__arrow_active');
+                    }
                     setTimeout(function () {
                         imageBlock.src = images[currentPosition].url;
                         imageBlock.classList.remove('disappear');
-                    }, 500);
-                    for (var i = 0; i < radioButtons.length; i++) {
-                        radioButtons[i].classList.remove('slider__button_active');
-                        }
-                        radioButtons[currentPosition].classList.add('slider__button_active');
-                    if (currentPosition === 0) {
-                        backButton.classList.remove('slider__arrow_active');
-                        } else if (currentPosition === radioButtons.length - 1) {
-                        forwardButton.classList.remove('slider__arrow_active');
-                        } else if (currentPosition !== 0 && currentPosition !== radioButtons.length - 1) {
-                        backButton.classList.add('slider__arrow_active');forwardButton.classList.add('slider__arrow_active');
-                        }
+                    },latency/2)
+                }
+
+                function throttle(func, ms) {
+                    if (!throttled) {
+                        func();
+                        throttled = true;
+                        setTimeout(function () {
+                            throttled = false
+                        }, ms);
+                    }
                 }
             }
         }
     };
 })();
-
-
-function first(callback) {
-    setTimeout(function() {console.log('first'), 100});
-    callback();
-}
-
-function second(a, b) {
-    console.log(a*b);
-}
-
-first(function () {
-    second(5, 5)
-});
